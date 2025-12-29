f190c2952eb2cbdb25a5d25749275fd49a1506eb
Pratham3156 <prathamsunesara@gmail.com>
2025-12-29 17:41:33 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -16,3 +16,8 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+// Import Firestore
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const db = getFirestore(app);
+export { db };
